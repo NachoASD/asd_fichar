1bb3063441a7e036d4551f9077905407e24e79d9
NachoASD <69275490+NachoASD@users.noreply.github.com>
2021-01-25 21:07:03 +0100

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,11 @@
+--------------------------
+---By NachoASD @2020   ---
+---NachoASD#5887       ---
+--------------------------
+
+Config               = {}
+Config.webhook       = "" --- La URL del webhook
+Config.image         = "" --- Vacio es = Ninguna imagen
+Config.policePos     = vector3(445.07, -979.64, 30.69)
+Config.taxiPos       = vector3(895.08, -179.25, 75.07)
+Config.ambulancePos  = vector3(307.09, -597.51, 43.28)
