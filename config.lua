7c929c6f9a3c0b0d2e4b8ea0978d1682d2f5db85
NachoASD <69275490+NachoASD@users.noreply.github.com>
2021-01-27 16:17:49 +0100

    Removed webhook of the client

--- config.lua
+++ config.lua
@@ -1,11 +1,9 @@
 --------------------------
----By NachoASD @2020   ---
+---By NachoASD @2021   ---
 ---NachoASD#5887       ---
 --------------------------
 
 Config               = {}
-Config.webhook       = "" --- La URL del webhook
-Config.image         = "" --- Vacio es = Ninguna imagen
 Config.policePos     = vector3(445.07, -979.64, 30.69)
 Config.taxiPos       = vector3(895.08, -179.25, 75.07)
 Config.ambulancePos  = vector3(307.09, -597.51, 43.28)
