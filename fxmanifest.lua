d01d54249b4c180cbc9028304eba7842a4f86fc8
NachoASD <69275490+NachoASD@users.noreply.github.com>
2021-01-27 16:16:32 +0100

    Update and rename __resource.lua to fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,22 +2,20 @@
 ---By NachoASD @2021   ---
 ---NachoASD#5887       ---
 --------------------------
+fx_version 'cerulean'
+game 'gta5'
 
 author 'NachoASD'
 description 'Un script para fichar'
-version '1.0.0'
-
-fx_version 'adamant'
-game 'gta5'
+version '1.1'
 
 shared_script 'config.lua'
+
 client_scripts {
-    "client.lua",
-    '@es_extended/locale.lua'
+    "client.lua"
 }
 server_scripts {
     "server.lua",
-    '@es_extended/locale.lua',
     "@mysql-async/lib/MySQL.lua"
 }
 
